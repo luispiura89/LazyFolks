4cc375e95de6bf242f21df7488ff6f992988920e
luispiura89 <luispiura@unitec.edu>
2021-11-19 14:52:27 -0600

    Create `LazyFolks` workspace

---
+++ LazyFolksEngine/LazyFolksEngine/LazyFolksEngine.h
@@ -0,0 +1,18 @@
+//
+//  LazyFolksEngine.h
+//  LazyFolksEngine
+//
+//  Created by Luis Francisco Piura Mejia on 19/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LazyFolksEngine.
+FOUNDATION_EXPORT double LazyFolksEngineVersionNumber;
+
+//! Project version string for LazyFolksEngine.
+FOUNDATION_EXPORT const unsigned char LazyFolksEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LazyFolksEngine/PublicHeader.h>
+
+
---
+++ LazyFolksiOSApp/LazyFolksiOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LazyFolksiOSApp
+//
+//  Created by Luis Francisco Piura Mejia on 19/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
