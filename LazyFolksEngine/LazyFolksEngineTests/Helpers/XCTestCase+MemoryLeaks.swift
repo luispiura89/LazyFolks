e0c2a43dac96c0245271f1c958a9e0b01b9374d6
luispiura89 <luispiura@unitec.edu>
2021-11-23 11:06:18 -0600

    ensure by tests that instances are correctly deallocated

---
+++ LazyFolksEngine/LazyFolksEngineTests/Helpers/XCTestCase+MemoryLeaks.swift
@@ -0,0 +1,21 @@
+//
+//  XCTestCase+MemoryLeaks.swift
+//  LazyFolksEngineTests
+//
+//  Created by Luis Francisco Piura Mejia on 23/11/21.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(
+                instance,
+                "Instance \(String(describing: instance)) should have been deallocated potential memory leak",
+                file: file,
+                line: line
+            )
+        }
+    }
+}
