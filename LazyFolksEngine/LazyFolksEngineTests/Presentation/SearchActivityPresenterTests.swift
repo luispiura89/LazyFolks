beef65e21ab9ca8370cc96ea02f501b9b243ee7b
luispiura89 <luispiura@unitec.edu>
2021-11-23 10:11:31 -0600

    `SearchActivityPresenter` provides localized strings

---
+++ LazyFolksEngine/LazyFolksEngineTests/Presentation/SearchActivityPresenterTests.swift
@@ -0,0 +1,33 @@
+//
+//  SearchActivityPresenterTests.swift
+//  LazyFolksEngineTests
+//
+//  Created by Luis Francisco Piura Mejia on 23/11/21.
+//
+
+import Foundation
+import LazyFolksEngine
+import XCTest
+
+final class SearchActivityPresenterTests: XCTestCase {
+    
+    func test_presenter_shouldLocalizeTitleAndPlaceholders() {
+        XCTAssertEqual(SearchActivityPresenter.title, localized("SEARCH_ACTIVITY_VIEW_TITLE"))
+        XCTAssertEqual(SearchActivityPresenter.subtitle, localized("SEARCH_ACTIVITY_VIEW_SUBTITLE"))
+        XCTAssertEqual(SearchActivityPresenter.typePlaceholder, localized("SEARCH_ACTIVITY_TYPE_PLACEHOLDER"))
+        XCTAssertEqual(SearchActivityPresenter.participantsPlaceholder, localized("SEARCH_ACTIVITY_PARTICIPANTS_PLACEHOLDER"))
+        XCTAssertEqual(SearchActivityPresenter.minPricePlaceholder, localized("SEARCH_ACTIVITY_MIN_PRICE_PLACEHOLDER"))
+        XCTAssertEqual(SearchActivityPresenter.maxPricePlaceholder, localized("SEARCH_ACTIVITY_MAX_PRICE_PLACEHOLDER"))
+    }
+    
+    // MARK: - Helpers
+    
+    private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+        let bundle = Bundle(for: SearchActivityPresenter.self)
+        let localized = bundle.localizedString(forKey: key, value: nil, table: "SearchActivity")
+        if localized == key {
+            XCTFail("Please provide a localization for the key \(key)", file: file, line: line)
+        }
+        return localized
+    }
+}
