66e442f1f68a5d8613813be0c0835b9ce6e56d3d
luispiura89 <luispiura@unitec.edu>
2021-11-24 16:40:44 -0600

    Compose `SearchActivityViewController` with a valid publisher

--- LazyFolksiOSApp/LazyFolksiOSApp/SceneDelegate.swift
+++ LazyFolksiOSApp/LazyFolksiOSApp/SceneDelegate.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import Combine
 import LazyFolksiOS
+import LazyFolksEngine
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    private let baseURL = URL(string: "https://www.boredapi.com/api/")!
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -21,38 +24,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: scene)
         let bounds = window!.bounds
-        window!.rootViewController = SearchViewComposer.compose(windowBounds: bounds)
+        window!.rootViewController = SearchViewComposer.compose(
+            windowBounds: bounds,
+            loader: loaderPublisher,
+            navigationHandler: { activity in
+                print(activity)
+            }
+        )
         window!.makeKeyAndVisible()
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    private func loaderPublisher(
+        type: String,
+        participants: String,
+        minPrice: String,
+        maxPrice: String
+    ) -> AnyPublisher<Activity, Error> {
+        let session = URLSession(configuration: .ephemeral)
+        let url = ActivityEndpoint.get(
+            type: type,
+            participants: participants,
+            minRange: minPrice,
+            maxRange: maxPrice
+        ).requestURL(baseURL: baseURL)
+        return URLSessionHTTPClient(session: session)
+            .getPublisher(from: url)
+            .dispatchOnMainQueue()
+            .tryMap(ActivitiesMapper.map)
+            .eraseToAnyPublisher()
     }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
